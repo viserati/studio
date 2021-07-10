57fca99cd07d78a42ff18ad11571d9f844e70411
Keith Turner <turner@keithturner.com>
2021-07-09 22:40:24 -0600

    New updates for Heroku deployment

--- studio_project/studio_project/settings.py
+++ studio_project/studio_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%rkq4_ptl7_6wsbo2(bkbi%j#1ep9f7f5#w$+mh$29arqd6s5u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
 
-                
+
             ],
         },
     },
